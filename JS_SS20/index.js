5a74b7ac0ee8d8946fd4cd3ccb88d29cc3b6457e
Hiepduong040 <147359314+Hiepduong040@users.noreply.github.com>
2024-01-11 06:52:05 +0700

    Add files via upload

---
+++ JS_SS20/index.js
@@ -0,0 +1,61 @@
+
+console. log("Hello world");
+console. log("ặidjiw");
+console. log("1ijdiaw");
+console. log("ạo2qj");
+
+//từ khóa
+// var, let, const
+var fullName = "Sợ người";
+let job = "Ăn trộm";
+const age = 20;
+
+console.log(fullName);
+console.log(job);
+console.log(age );
+
+
+
+// var answer = prompt("Xin mời mày nhập tên vào ô này:");
+
+// console.log(answer);
+
+//input - prompt
+
+let a = -prompt("xin mời nhập vào số a:");
+let b = -prompt("xin mời nhập vào số b:");
+
+let Sum = a - b;
+
+document.write("Tổng hai số là: " + Sum);
+
+
+//out - console.log
+
+//Cho người dùng nhập vào 2 số a và b
+
+//Tính tổng 2 số đó và in tổng đó ra màn hình console
+
+console.log("Xin chào")
+console.log('Hello'
+)
+
+var a1 = "Alo 123 123";
+var b1 = `Xin chào ABC ABC`;
+console.log(a1);
+console.log(b1);
+
+console.log(a1 + " " + b1);
+
+var fname = "Anh Quân";
+console.log(`Xin chào tôi tên là ${fname}, Tôi bị nghiện`);
+
+let number = 1000000000;
+console.log(number);
+
+console.log(a ** b);
+
+//1. 3 từ khóa khai báo biến var, let, const khác gì nhau???
+//2. Trình bày quy tắc đặt tên biến (chữ cái đầu,...)
+//3. Thế nào là cơ chế hoisting trong Javascript
+//
